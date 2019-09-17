5f5dad3172857d097f442b4f79232066aa5487ba
Nick Barrett <nickbarrett@protonmail.com>
2019-09-17 13:07:41 -0500

    Session 4

---
+++ Session04/L4.py
@@ -0,0 +1,5 @@
+# Programer: Nick Barrett
+# Date Written: Sep 17, 2019
+# Program Name: L4.py
+# Company Name: HTC-CCIS1505
+
---
+++ Session04/P4.py
@@ -0,0 +1,19 @@
+# Programer: Nick Barrett
+# Date Written: Sep 17, 2019
+# Program Name: P4.py
+# Company Name: HTC-CCIS1505
+
+
+# step 2
+
+
+# step 3
+
+
+# step 4
+
+
+# step 5
+
+
+# step 6
