1478184bbeebc70215125394eff55e04eec07890
Nick Barrett <nickbarrett@protonmail.com>
2019-09-17 14:19:36 -0500

    notes

--- Session04/L4.py
+++ Session04/L4.py
@@ -3,3 +3,31 @@
 # Program Name: L4.py
 # Company Name: HTC-CCIS1505
 
+##password = "secret"
+##
+##if password == "secret":
+##    print("You're In!")
+
+##strName = input("Enter first name: ")
+##if strName == "steve": #if statement
+##    print("Hi Steve!") #true block
+##    print("have a great day!")
+##    print("TRUE BLOCK")
+##else:
+##    print("You are not steve!")#false block
+##    print("have a bad day!")
+##    print("FALSE BLOCK")
+
+
+strChoice = input("Enter your choice: ")
+if strChoice == "1":
+    print("Enjoy 1")
+elif strChoice == "2":
+    print("Enjoy 2")
+elif strChoice == "3":
+    print("Enjoy 3")
+elif strChoice == "4":
+    print("Enjoy 4")
+else:
+    print("STARVE!")
+
