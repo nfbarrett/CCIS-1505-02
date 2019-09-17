4f830fbead78be3ca9258722375a326c73b4ff68
Nick Barrett <nickbarrett@protonmail.com>
2019-09-17 15:32:23 -0500

    Update P4.py

--- Session04/P4.py
+++ Session04/P4.py
@@ -8,21 +8,21 @@
 print("\nstep 2 1 to 10 by 1")
 int_Counter = 1
 while int_Counter <= 10:
-    print(int_Counter)
+    print("Loop Counter is: ", int_Counter)
     int_Counter += 1
 
 # step 3
 print("\nstep 3 1 to 101 by 10")
 int_Counter = 1
 while int_Counter <= 101:
-    print(int_Counter)
+    print("Loop Counter is: ", int_Counter)
     int_Counter += 10
 
 # step 4
 print("\nstep 4 1000 down to 0 by 100")
 int_Counter = 1000
 while int_Counter >= 0:
-    print(int_Counter)
+    print("Loop Counter is: ", int_Counter)
     int_Counter -= 100
 
 # step 5
